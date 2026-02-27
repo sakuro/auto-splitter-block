08ff8ec2b582a26ec100ad3eea20bbecf54715fe
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2026-02-27 10:13:24 +0900

    :sparkles: Use no-item as default block filter item
    The base game's hidden no-item provides a cleaner appearance
    than deconstruction-planner when used as a splitter block filter.
    
    Closes #8

--- settings.lua
+++ settings.lua
@@ -2,8 +2,8 @@ local setting = {
   type = "string-setting",
   name = "auto-splitter-block-filter-item",
   setting_type = "startup",
-  default_value = "deconstruction-planner",
-  allowed_values = {"deconstruction-planner"},
+  default_value = "no-item",
+  allowed_values = {"no-item", "deconstruction-planner"},
   order = "a",
 }
 
@@ -11,7 +11,5 @@ if mods["atan-null"] then
   table.insert(setting.allowed_values, "atan-null")
   setting.default_value = "atan-null"
 end
-
-setting.hidden = #setting.allowed_values <= 1
 
 data:extend({setting})
